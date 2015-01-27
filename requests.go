e50b253ddfd6ef49cc947cdb8f2135a3da85f897
AmarShaked <shakedamar@gmail.com>
2015-01-27 14:02:13 +0200

    Add the OPTIONS, PUT, HEAD, DELETE quick functions

--- requests.go
+++ requests.go
@@ -12,28 +12,64 @@ import (
 
 var DefaultClient = &http.Client{}
 
+// Get send a quick GET http request.
 func Get(url string) (*Response, error) {
 
-	resp, err := DefaultClient.Get(url)
-
-	if err != nil {
-		return nil, err
-	}
-
-	r := setNewResponse(resp)
-
-	err = bodyToBytes(r)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return r, nil
+	return responseHandler(DefaultClient.Get(url))
 }
 
+// Post send a quick POST http request.
 func Post(url string, data map[string]string) (*Response, error) {
 
-	resp, err := DefaultClient.Post(url, "application/x-www-form-urlencoded", strings.NewReader(encode(data)))
+	return responseHandler(DefaultClient.Post(url, "application/x-www-form-urlencoded", strings.NewReader(encode(data))))
+}
+
+// Head send a quick HEAD http request.
+func Head(url string) (*Response, error) {
+
+	return responseHandler(DefaultClient.Head(url))
+}
+
+// Put send a quick PUT http request.
+func Put(url string, data map[string]string) (*Response, error) {
+
+	req, err := http.NewRequest("PUT", url, strings.NewReader(encode(data)))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return responseHandler(DefaultClient.Do(req))
+
+}
+
+// Delete send a quick DELETE http request.
+func Delete(url string, data map[string]string) (*Response, error) {
+
+	req, err := http.NewRequest("DELETE", url, strings.NewReader(encode(data)))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return responseHandler(DefaultClient.Do(req))
+}
+
+// Options send a quick OPTIONS http request.
+func Options(url string) (*Response, error) {
+
+	req, err := http.NewRequest("OPTIONS", url, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return responseHandler(DefaultClient.Do(req))
+}
+
+// This function handle all the logic after we get a http.Request.
+// Handle all the errors and set the new Response object
+func responseHandler(resp *http.Response, err error) (*Response, error) {
 
 	if err != nil {
 		return nil, err
@@ -41,6 +77,7 @@ func Post(url string, data map[string]string) (*Response, error) {
 
 	r := setNewResponse(resp)
 
+	// Read the body and saveit as bytes
 	err = bodyToBytes(r)
 
 	if err != nil {
@@ -51,6 +88,7 @@ func Post(url string, data map[string]string) (*Response, error) {
 }
 
 // This function encode the map to formData
+// {"exs": "shaked", "exs2": "shaked2"} == "exs=shaked&exs2=shaked2"
 func encode(v map[string]string) string {
 
 	if v == nil {
