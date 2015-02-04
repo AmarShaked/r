df25532cb88e37577918154280bf88e04a78c743
AmarShaked <shakedamar@gmail.com>
2015-02-04 16:05:52 +0200

    Add more properties and function to the response type

--- response.go
+++ response.go
@@ -17,17 +17,52 @@ type Response struct {
 	Request *http.Request
 
 	// The value is the BaseResponse Body as bytes
-	// Used by the Text function and the Json function.
-	bytes []byte
+	Bytes []byte
 
 	// The http.Response type, This type is the original type
 	// That return from the server.
 	BaseResponse *http.Response
+
+	Close bool
+
+	// The Final URL location.
+	Url string
+
+	// The response status as boolean type
+	Ok bool
+}
+
+// NewResponse get a http.Response and return a Response type.
+func NewResponse(resp *http.Response) (*Response, error) {
+
+	r := &Response{
+		BaseResponse: resp,
+		StatusCode:   resp.StatusCode,
+		Status:       resp.Status,
+		Proto:        resp.Proto,
+		Request:      resp.Request,
+		Close:        resp.Close,
+
+		// Return the url as string type.
+		Url: resp.Request.URL.String(),
+
+		// Return the status code as boolean value.
+		Ok: resp.StatusCode >= 400,
+	}
+
+	// Read the body and save it as bytes
+	err := r.responseBodyToBytes()
+
+	if err != nil {
+		return r, err
+	}
+
+	return r, nil
 }
 
 // Convert the request body(io.Reader) to bytes
 // Becouse the io.Reader.Close function
-func responseBodyToBytes(r *Response) error {
+func (r *Response) responseBodyToBytes() error {
 
 	defer r.BaseResponse.Body.Close()
 	body, err := ioutil.ReadAll(r.BaseResponse.Body)
@@ -37,18 +72,19 @@ func responseBodyToBytes(r *Response) error {
 	}
 
 	// Save the body for future usage
-	r.bytes = body
+	r.Bytes = body
 
 	return nil
 }
 
 // Get the response body as a string
 func (r *Response) Text() string {
-	return string(r.bytes)
+	return string(r.Bytes)
 }
 
 // Get response header value by name
 func (r *Response) Headers(key string) string {
+
 	return r.BaseResponse.Header.Get(key)
 }
 
@@ -56,7 +92,7 @@ func (r *Response) Headers(key string) string {
 // Used the json.Unmarshal function.
 func (r *Response) Json(v interface{}) error {
 
-	err := json.Unmarshal(r.bytes, v)
+	err := json.Unmarshal(r.Bytes, v)
 
 	if err != nil {
 		return err
